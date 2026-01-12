d70bb4a2c724d7be6ea2f57ecdff705596d806b4
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-12 14:47:55 -0500

    eventClasses showed logicalStrict missing, nice

--- packages/plugin-node/src/rules/ruleCreator.ts
+++ packages/plugin-node/src/rules/ruleCreator.ts
@@ -3,5 +3,5 @@ import { RuleCreator } from "@flint.fyi/core";
 export const ruleCreator = new RuleCreator({
 	docs: (ruleId) => `https://flint.fyi/rules/node/${ruleId.toLowerCase()}`,
 	pluginId: "node",
-	presets: ["logical", "stylistic", "stylisticStrict"],
+	presets: ["logical", "logicalStrict", "stylistic", "stylisticStrict"],
 });
