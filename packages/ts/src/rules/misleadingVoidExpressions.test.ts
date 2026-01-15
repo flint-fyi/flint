28e059260bc0c0ce75fd7faac7dc4e77907615f8
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-15 08:37:10 -0500

    touchups

--- packages/ts/src/rules/misleadingVoidExpressions.test.ts
+++ packages/ts/src/rules/misleadingVoidExpressions.test.ts
@@ -239,6 +239,449 @@ function run() {
 				},
 			],
 		},
+		{
+			code: `const x = console?.log('foo');`,
+			snapshot:
+				"const x = console?.log('foo');\n" +
+				"          ~~~~~~~~~~~~~~~~~~~\n" +
+				"          Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `const x = void console?.log('foo');`,
+				},
+			],
+		},
+		{
+			code: `console.error(console.log('foo'));`,
+			snapshot:
+				"console.error(console.log('foo'));\n" +
+				"              ~~~~~~~~~~~~~~~~~~\n" +
+				"              Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `console.error(void console.log('foo'));`,
+				},
+			],
+		},
+		{
+			code: `[console.log('foo')];`,
+			snapshot:
+				"[console.log('foo')];\n" +
+				" ~~~~~~~~~~~~~~~~~~\n" +
+				" Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `[void console.log('foo')];`,
+				},
+			],
+		},
+		{
+			code: `({ x: console.log('foo') });`,
+			snapshot:
+				"({ x: console.log('foo') });\n" +
+				"      ~~~~~~~~~~~~~~~~~~\n" +
+				"      Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `({ x: void console.log('foo') });`,
+				},
+			],
+		},
+		{
+			code: `console.log('foo') ? true : false;`,
+			snapshot:
+				"console.log('foo') ? true : false;\n" +
+				"~~~~~~~~~~~~~~~~~~\n" +
+				"Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `void console.log('foo') ? true : false;`,
+				},
+			],
+		},
+		{
+			code: `(console.log('foo') && true) || false;`,
+			snapshot:
+				"(console.log('foo') && true) || false;\n" +
+				" ~~~~~~~~~~~~~~~~~~\n" +
+				" Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `(void console.log('foo') && true) || false;`,
+				},
+			],
+		},
+		{
+			code: `!console.log('foo');`,
+			snapshot:
+				"!console.log('foo');\n" +
+				" ~~~~~~~~~~~~~~~~~~\n" +
+				" Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `!void console.log('foo');`,
+				},
+			],
+		},
+		{
+			code: `!!console.log('foo');`,
+			snapshot:
+				"!!console.log('foo');\n" +
+				"  ~~~~~~~~~~~~~~~~~~\n" +
+				"  Void expressions should not be used as values.",
+			suggestions: [
+				{
+					id: "wrapWithVoid",
+					updated: `!!void console.log('foo');`,
+				},
+			],
+		},
+		{
+			code: `
+function notcool(input: string) {
+	return (input, console.log(input));
+}
+`,
+			snapshot: `
+function notcool(input: string) {
+	return (input, console.log(input));
+	               ~~~~~~~~~~~~~~~~~~
+	               Returning a void expression from a function is misleading.
+}
+`,
+			suggestions: [
+				{
+					id: "removeReturn",
+					updated: `
+function notcool(input: string) {
+	(input, console.log(input));
+}
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+function notcool(input: string) {
+	return (input, void console.log(input));
+}
+`,
+				},
+			],
+		},
+		{
+			code: `foo => (foo ? console.log(true) : console.log(false));`,
+			snapshot:
+				"foo => (foo ? console.log(true) : console.log(false));\n" +
+				"              ~~~~~~~~~~~~~~~~~\n" +
+				"              Returning a void expression from an arrow function shorthand is misleading.\n" +
+				"                                  ~~~~~~~~~~~~~~~~~~\n" +
+				"                                  Returning a void expression from an arrow function shorthand is misleading.",
+			suggestions: [
+				{
+					id: "addBraces",
+					updated: `foo => { (foo ? console.log(true) : console.log(false)); };`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `foo => (foo ? void console.log(true) : console.log(false));`,
+				},
+				{
+					id: "addBraces",
+					updated: `foo => { (foo ? console.log(true) : console.log(false)); };`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `foo => (foo ? console.log(true) : void console.log(false));`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	if (cond) {
+		return console.error('foo');
+	}
+	console.log('bar');
+};
+`,
+			snapshot: `
+const f = function () {
+	if (cond) {
+		return console.error('foo');
+		       ~~~~~~~~~~~~~~~~~~~~
+		       Returning a void expression from a function is misleading.
+	}
+	console.log('bar');
+};
+`,
+			suggestions: [
+				{
+					id: "moveBeforeReturn",
+					updated: `
+const f = function () {
+	if (cond) {
+		console.error('foo'); return;
+	}
+	console.log('bar');
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	if (cond) {
+		return void console.error('foo');
+	}
+	console.log('bar');
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	if (cond) return console.error('foo');
+	console.log('bar');
+};
+`,
+			snapshot: `
+const f = function () {
+	if (cond) return console.error('foo');
+	                 ~~~~~~~~~~~~~~~~~~~~
+	                 Returning a void expression from a function is misleading.
+	console.log('bar');
+};
+`,
+			suggestions: [
+				{
+					id: "moveBeforeReturn",
+					updated: `
+const f = function () {
+	if (cond) console.error('foo'); return;
+	console.log('bar');
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	if (cond) return void console.error('foo');
+	console.log('bar');
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	let num = 1;
+	return num ? console.log('foo') : num;
+};
+`,
+			snapshot: `
+const f = function () {
+	let num = 1;
+	return num ? console.log('foo') : num;
+	             ~~~~~~~~~~~~~~~~~~
+	             Returning a void expression from a function is misleading.
+};
+`,
+			suggestions: [
+				{
+					id: "removeReturn",
+					updated: `
+const f = function () {
+	let num = 1;
+	num ? console.log('foo') : num;
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	let num = 1;
+	return num ? void console.log('foo') : num;
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	let undef = undefined;
+	return undef ? console.log('foo') : undef;
+};
+`,
+			snapshot: `
+const f = function () {
+	let undef = undefined;
+	return undef ? console.log('foo') : undef;
+	               ~~~~~~~~~~~~~~~~~~
+	               Returning a void expression from a function is misleading.
+};
+`,
+			suggestions: [
+				{
+					id: "removeReturn",
+					updated: `
+const f = function () {
+	let undef = undefined;
+	undef ? console.log('foo') : undef;
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	let undef = undefined;
+	return undef ? void console.log('foo') : undef;
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	let num = 1;
+	return num || console.log('foo');
+};
+`,
+			snapshot: `
+const f = function () {
+	let num = 1;
+	return num || console.log('foo');
+	              ~~~~~~~~~~~~~~~~~~
+	              Returning a void expression from a function is misleading.
+};
+`,
+			suggestions: [
+				{
+					id: "removeReturn",
+					updated: `
+const f = function () {
+	let num = 1;
+	num || console.log('foo');
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	let num = 1;
+	return num || void console.log('foo');
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+const f = function () {
+	let bar = void 0;
+	return bar || console.log('foo');
+};
+`,
+			snapshot: `
+const f = function () {
+	let bar = void 0;
+	return bar || console.log('foo');
+	              ~~~~~~~~~~~~~~~~~~
+	              Returning a void expression from a function is misleading.
+};
+`,
+			suggestions: [
+				{
+					id: "removeReturn",
+					updated: `
+const f = function () {
+	let bar = void 0;
+	bar || console.log('foo');
+};
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+const f = function () {
+	let bar = void 0;
+	return bar || void console.log('foo');
+};
+`,
+				},
+			],
+		},
+		{
+			code: `
+let num = 1;
+const foo = () => (num ? console.log('foo') : num);
+`,
+			snapshot: `
+let num = 1;
+const foo = () => (num ? console.log('foo') : num);
+                         ~~~~~~~~~~~~~~~~~~
+                         Returning a void expression from an arrow function shorthand is misleading.
+`,
+			suggestions: [
+				{
+					id: "addBraces",
+					updated: `
+let num = 1;
+const foo = () => { (num ? console.log('foo') : num); };
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+let num = 1;
+const foo = () => (num ? void console.log('foo') : num);
+`,
+				},
+			],
+		},
+		{
+			code: `
+let bar = void 0;
+const foo = () => (bar ? console.log('foo') : bar);
+`,
+			snapshot: `
+let bar = void 0;
+const foo = () => (bar ? console.log('foo') : bar);
+                         ~~~~~~~~~~~~~~~~~~
+                         Returning a void expression from an arrow function shorthand is misleading.
+`,
+			suggestions: [
+				{
+					id: "addBraces",
+					updated: `
+let bar = void 0;
+const foo = () => { (bar ? console.log('foo') : bar); };
+`,
+				},
+				{
+					id: "wrapWithVoid",
+					updated: `
+let bar = void 0;
+const foo = () => (bar ? void console.log('foo') : bar);
+`,
+				},
+			],
+		},
 	],
 	valid: [
 		`declare function log(message: string): void; log("hello");`,
@@ -251,5 +694,17 @@ function run() {
 		`declare function getValue(): string | undefined; const x = getValue();`,
 		`declare function log(message: string): void; const items = [1, 2, 3]; items.forEach(n => { log(String(n)); });`,
 		`declare function log(message: string): void; (log("first"), log("second"));`,
+		`() => Math.random();`,
+		`declare function log(message: string): void; log?.("hello");`,
+		`
+function cool(input: string) {
+	return (console.log(input), input);
+}
+`,
+		`
+function cool(input: string) {
+	return (input, console.log(input), input);
+}
+`,
 	],
 });
--- packages/ts/src/rules/misleadingVoidExpressions.ts
+++ packages/ts/src/rules/misleadingVoidExpressions.ts
@@ -2,73 +2,17 @@ import * as tsutils from "ts-api-utils";
 import * as ts from "typescript";
 
 import { getTSNodeRange } from "../getTSNodeRange.ts";
-import { typescriptLanguage } from "../language.ts";
+import {
+	type TypeScriptFileServices,
+	typescriptLanguage,
+} from "../language.ts";
 import type * as AST from "../types/ast.ts";
-import type { Checker } from "../types/checker.ts";
 import { ruleCreator } from "./ruleCreator.ts";
 import { getConstrainedTypeAtLocation } from "./utils/getConstrainedType.ts";
 
-function isVoidLike(type: ts.Type) {
-	return tsutils.isTypeFlagSet(type, ts.TypeFlags.VoidLike);
-}
-
-function isInValidPosition(
-	node: ts.Node,
-): { valid: true } | { invalidAncestor: ts.Node; valid: false } {
-	const parent = node.parent;
-
-	if (ts.isExpressionStatement(parent)) {
-		return { valid: true };
-	}
-
-	if (
-		ts.isBinaryExpression(parent) &&
-		parent.operatorToken.kind === ts.SyntaxKind.CommaToken
-	) {
-		if (parent.right === node) {
-			return isInValidPosition(parent);
-		}
-		return { valid: true };
-	}
-
-	if (ts.isParenthesizedExpression(parent)) {
-		return isInValidPosition(parent);
-	}
-
-	if (
-		ts.isConditionalExpression(parent) &&
-		(parent.whenTrue === node || parent.whenFalse === node)
-	) {
-		return isInValidPosition(parent);
-	}
-
-	if (
-		ts.isBinaryExpression(parent) &&
-		(parent.operatorToken.kind === ts.SyntaxKind.AmpersandAmpersandToken ||
-			parent.operatorToken.kind === ts.SyntaxKind.BarBarToken ||
-			parent.operatorToken.kind === ts.SyntaxKind.QuestionQuestionToken) &&
-		parent.right === node
-	) {
-		return isInValidPosition(parent);
-	}
-
-	if (ts.isVoidExpression(parent)) {
-		return { valid: true };
-	}
-
-	if (ts.isArrowFunction(parent) && parent.body === node) {
-		return { invalidAncestor: parent, valid: false };
-	}
-
-	if (ts.isReturnStatement(parent)) {
-		return { invalidAncestor: parent, valid: false };
-	}
-
-	return { invalidAncestor: node, valid: false };
-}
-
 function getParentFunction(node: ts.Node) {
 	let current: ts.Node | undefined = node.parent;
+
 	while (current) {
 		if (
 			ts.isFunctionDeclaration(current) ||
@@ -78,9 +22,67 @@ function getParentFunction(node: ts.Node) {
 		) {
 			return current;
 		}
-		current = current.parent;
+		current = current.parent as ts.Node | undefined;
 	}
+
 	return undefined;
+}
+
+function isInValidPosition(
+	node: ts.Node,
+): { invalidAncestor: ts.Node; valid: false } | { valid: true } {
+	if (ts.isExpressionStatement(node.parent)) {
+		return { valid: true };
+	}
+
+	if (
+		ts.isBinaryExpression(node.parent) &&
+		node.parent.operatorToken.kind === ts.SyntaxKind.CommaToken
+	) {
+		if (node.parent.right === node) {
+			return isInValidPosition(node.parent);
+		}
+		return { valid: true };
+	}
+
+	if (ts.isParenthesizedExpression(node.parent)) {
+		return isInValidPosition(node.parent);
+	}
+
+	if (
+		ts.isConditionalExpression(node.parent) &&
+		(node.parent.whenTrue === node || node.parent.whenFalse === node)
+	) {
+		return isInValidPosition(node.parent);
+	}
+
+	if (
+		ts.isBinaryExpression(node.parent) &&
+		(node.parent.operatorToken.kind === ts.SyntaxKind.AmpersandAmpersandToken ||
+			node.parent.operatorToken.kind === ts.SyntaxKind.BarBarToken ||
+			node.parent.operatorToken.kind === ts.SyntaxKind.QuestionQuestionToken) &&
+		node.parent.right === node
+	) {
+		return isInValidPosition(node.parent);
+	}
+
+	if (ts.isVoidExpression(node.parent)) {
+		return { valid: true };
+	}
+
+	if (ts.isArrowFunction(node.parent) && node.parent.body === node) {
+		return { invalidAncestor: node.parent, valid: false };
+	}
+
+	if (ts.isReturnStatement(node.parent)) {
+		return { invalidAncestor: node.parent, valid: false };
+	}
+
+	return { invalidAncestor: node, valid: false };
+}
+
+function isVoidLike(type: ts.Type) {
+	return tsutils.isTypeFlagSet(type, ts.TypeFlags.VoidLike);
 }
 
 export default ruleCreator.createRule(typescriptLanguage, {
@@ -91,17 +93,6 @@ export default ruleCreator.createRule(typescriptLanguage, {
 		presets: ["logical"],
 	},
 	messages: {
-		voidExpressionValue: {
-			primary: "Void expressions should not be used as values.",
-			secondary: [
-				"A `void` expression evaluates to `undefined` but its intent is to be ignored.",
-				"Using void expressions as values can lead to confusing code and potential bugs.",
-			],
-			suggestions: [
-				"Move the void expression to its own statement.",
-				"Wrap with `void` operator to indicate the value is intentionally ignored.",
-			],
-		},
 		voidExpressionArrow: {
 			primary:
 				"Returning a void expression from an arrow function shorthand is misleading.",
@@ -124,15 +115,25 @@ export default ruleCreator.createRule(typescriptLanguage, {
 				"Remove the return keyword if this is the last statement.",
 			],
 		},
+		voidExpressionValue: {
+			primary: "Void expressions should not be used as values.",
+			secondary: [
+				"A `void` expression evaluates to `undefined` but its intent is to be ignored.",
+				"Using void expressions as values can lead to confusing code and potential bugs.",
+			],
+			suggestions: [
+				"Move the void expression to its own statement.",
+				"Wrap with `void` operator to indicate the value is intentionally ignored.",
+			],
+		},
 	},
 	setup(context) {
 		function checkVoidExpression(
 			node:
-				| AST.CallExpression
 				| AST.AwaitExpression
+				| AST.CallExpression
 				| AST.TaggedTemplateExpression,
-			typeChecker: Checker,
-			sourceFile: ts.SourceFile,
+			{ sourceFile, typeChecker }: TypeScriptFileServices,
 		) {
 			const type = getConstrainedTypeAtLocation(node, typeChecker);
 
@@ -149,6 +150,10 @@ export default ruleCreator.createRule(typescriptLanguage, {
 
 			if (ts.isArrowFunction(invalidAncestor)) {
 				const arrowBodyText = sourceFile.text.slice(
+					invalidAncestor.body.getStart(sourceFile),
+					invalidAncestor.body.getEnd(),
+				);
+				const nodeText = sourceFile.text.slice(
 					node.getStart(sourceFile),
 					node.getEnd(),
 				);
@@ -164,12 +169,22 @@ export default ruleCreator.createRule(typescriptLanguage, {
 						{
 							id: "wrapWithVoid",
 							range: getTSNodeRange(node, sourceFile),
-							text: `void ${arrowBodyText}`,
+							text: `void ${nodeText}`,
 						},
 					],
 				});
 				return;
 			}
+
+			const nodeText = sourceFile.text.slice(
+				node.getStart(sourceFile),
+				node.getEnd(),
+			);
+			const suggestionBase = {
+				id: "wrapWithVoid",
+				range: getTSNodeRange(node, sourceFile),
+				text: `void ${nodeText}`,
+			};
 
 			if (ts.isReturnStatement(invalidAncestor)) {
 				const functionNode = getParentFunction(invalidAncestor);
@@ -188,72 +203,40 @@ export default ruleCreator.createRule(typescriptLanguage, {
 						)
 					: "";
 
-				if (isLastStatement) {
-					context.report({
-						message: "voidExpressionReturn",
-						range: getTSNodeRange(node, sourceFile),
-						suggestions: [
-							{
-								id: "removeReturn",
-								range: getTSNodeRange(invalidAncestor, sourceFile),
-								text: `${returnValueText};`,
-							},
-							{
-								id: "wrapWithVoid",
-								range: getTSNodeRange(node, sourceFile),
-								text: `void ${returnValueText}`,
-							},
-						],
-					});
-				} else {
-					context.report({
-						message: "voidExpressionReturn",
-						range: getTSNodeRange(node, sourceFile),
-						suggestions: [
-							{
-								id: "moveBeforeReturn",
-								range: getTSNodeRange(invalidAncestor, sourceFile),
-								text: `${returnValueText}; return;`,
-							},
-							{
-								id: "wrapWithVoid",
-								range: getTSNodeRange(node, sourceFile),
-								text: `void ${returnValueText}`,
-							},
-						],
-					});
-				}
+				context.report({
+					message: "voidExpressionReturn",
+					range: getTSNodeRange(node, sourceFile),
+					suggestions: [
+						isLastStatement
+							? {
+									id: "removeReturn",
+									range: getTSNodeRange(invalidAncestor, sourceFile),
+									text: `${returnValueText};`,
+								}
+							: {
+									id: "moveBeforeReturn",
+									range: getTSNodeRange(invalidAncestor, sourceFile),
+									text: `${returnValueText}; return;`,
+								},
+						suggestionBase,
+					],
+				});
+
 				return;
 			}
 
-			const nodeText = sourceFile.text.slice(
-				node.getStart(sourceFile),
-				node.getEnd(),
-			);
 			context.report({
 				message: "voidExpressionValue",
 				range: getTSNodeRange(node, sourceFile),
-				suggestions: [
-					{
-						id: "wrapWithVoid",
-						range: getTSNodeRange(node, sourceFile),
-						text: `void ${nodeText}`,
-					},
-				],
+				suggestions: [suggestionBase],
 			});
 		}
 
 		return {
 			visitors: {
-				AwaitExpression: (node, { sourceFile, typeChecker }) => {
-					checkVoidExpression(node, typeChecker, sourceFile);
-				},
-				CallExpression: (node, { sourceFile, typeChecker }) => {
-					checkVoidExpression(node, typeChecker, sourceFile);
-				},
-				TaggedTemplateExpression: (node, { sourceFile, typeChecker }) => {
-					checkVoidExpression(node, typeChecker, sourceFile);
-				},
+				AwaitExpression: checkVoidExpression,
+				CallExpression: checkVoidExpression,
+				TaggedTemplateExpression: checkVoidExpression,
 			},
 		};
 	},
