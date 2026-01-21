e72ed2c5cdea39bb48dff262227c39bba04b29d6
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-21 13:04:46 -0500

    feat(ts): implement optionalChainOperators rule
    Basic implementation that detects simple patterns where optional chaining can be used:
    - foo && foo.bar → foo?.bar
    - foo.bar && foo.bar.baz → foo.bar?.baz
    
    Handles && (logical AND) patterns. Fixes #1812.

--- packages/ts/src/plugin.ts
+++ packages/ts/src/plugin.ts
@@ -151,6 +151,7 @@ import objectProto from "./rules/objectProto.ts";
 import objectPrototypeBuiltIns from "./rules/objectPrototypeBuiltIns.ts";
 import octalEscapes from "./rules/octalEscapes.ts";
 import octalNumbers from "./rules/octalNumbers.ts";
+import optionalChainOperators from "./rules/optionalChainOperators.ts";
 import parameterReassignments from "./rules/parameterReassignments.ts";
 import returnAssignments from "./rules/returnAssignments.ts";
 import selfAssignments from "./rules/selfAssignments.ts";
@@ -330,6 +331,7 @@ export const ts = createPlugin({
 		objectPrototypeBuiltIns,
 		octalEscapes,
 		octalNumbers,
+		optionalChainOperators,
 		parameterReassignments,
 		returnAssignments,
 		selfAssignments,
