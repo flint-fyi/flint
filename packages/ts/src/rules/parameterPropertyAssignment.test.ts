a77fc8bd768dd2ffeab46f2ef456eb1ba65fa9c3
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-22 13:02:31 -0500

    More comprehensive

--- packages/ts/src/rules/parameterPropertyAssignment.test.ts
+++ packages/ts/src/rules/parameterPropertyAssignment.test.ts
@@ -5,36 +5,286 @@ ruleTester.describe(rule, {
 	invalid: [
 		{
 			code: `
-class MyClass {
-	constructor(value: string) {
-		this.value = value;
+class Foo {
+	constructor(public foo: string) {
+		this.foo = foo;
 	}
 }
 `,
 			snapshot: `
-class MyClass {
-	constructor(value: string) {
-		this.value = value;
-		~~~~~~~~~~~~~~~~~~
-		Parameter property assignment is unnecessary.
+class Foo {
+	constructor(public foo: string) {
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
 	}
 }
 `,
 		},
 		{
 			code: `
-class Service {
-	update(data: object) {
-		this.data = data;
+class Foo {
+	constructor(public foo?: string) {
+		this.foo = foo!;
 	}
 }
 `,
 			snapshot: `
-class Service {
-	update(data: object) {
-		this.data = data;
+class Foo {
+	constructor(public foo?: string) {
+		this.foo = foo!;
+		~~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo?: string) {
+		this.foo = foo as any;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo?: string) {
+		this.foo = foo as any;
+		~~~~~~~~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo = '') {
+		this.foo = foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo = '') {
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo = '') {
+		this.foo = foo;
+		this.foo += 'foo';
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo = '') {
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+		this.foo += 'foo';
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo ||= foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo ||= foo;
 		~~~~~~~~~~~~~~~~
-		Parameter property assignment is unnecessary.
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo ??= foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo ??= foo;
+		~~~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo &&= foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(public foo: string) {
+		this.foo &&= foo;
+		~~~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		this['foo'] = foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		this['foo'] = foo;
+		~~~~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		function bar() {
+			this.foo = foo;
+		}
+		this.foo = foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		function bar() {
+			this.foo = foo;
+		}
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		this.bar = () => {
+			this.foo = foo;
+		};
+		this.foo = foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		this.bar = () => {
+			this.foo = foo;
+		};
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		class Bar {
+			constructor(private foo: string) {
+				this.foo = foo;
+			}
+		}
+		this.foo = foo;
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		class Bar {
+			constructor(private foo: string) {
+				this.foo = foo;
+				~~~~~~~~~~~~~~
+				This assignment is unnecessary since it is already assigned by a parameter property.
+			}
+		}
+		this.foo = foo;
+		~~~~~~~~~~~~~~
+		This assignment is unnecessary since it is already assigned by a parameter property.
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		{
+			this.foo = foo;
+		}
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		{
+			this.foo = foo;
+			~~~~~~~~~~~~~~
+			This assignment is unnecessary since it is already assigned by a parameter property.
+		}
+	}
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+	constructor(private foo: string) {
+		(() => {
+			this.foo = foo;
+		})();
+	}
+}
+`,
+			snapshot: `
+class Foo {
+	constructor(private foo: string) {
+		(() => {
+			this.foo = foo;
+			~~~~~~~~~~~~~~
+			This assignment is unnecessary since it is already assigned by a parameter property.
+		})();
 	}
 }
 `,
@@ -42,15 +292,118 @@ class Service {
 	],
 	valid: [
 		`
-class MyClass {
-	constructor(value: string) {
-		this.other = value;
+class Foo {
+	constructor(foo: string) {}
+}
+`,
+		`
+class Foo {
+	constructor(private foo: string) {}
+}
+`,
+		`
+class Foo {
+	constructor(private foo: string) {
+		this.foo = bar;
 	}
 }
 `,
 		`
-function fn(x: number) {
-	this.x = x;
+class Foo {
+	constructor(private foo: any) {
+		this.foo = foo.bar;
+	}
+}
+`,
+		`
+class Foo {
+	constructor(private foo: string) {
+		this.foo = this.bar;
+	}
+}
+`,
+		`
+class Foo {
+	foo: string;
+	constructor(foo: string) {
+		this.foo = foo;
+	}
+}
+`,
+		`
+class Foo {
+	bar: string;
+	constructor(private foo: string) {
+		this.bar = foo;
+	}
+}
+`,
+		`
+class Foo {
+	constructor(private foo: string) {
+		this.bar = () => {
+			this.foo = foo;
+		};
+	}
+}
+`,
+		`
+class Foo {
+	constructor(private foo: string) {
+		this[\`\${foo}\`] = foo;
+	}
+}
+`,
+		`
+function Foo(foo) {
+	this.foo = foo;
+}
+`,
+		`
+const foo = 'foo';
+this.foo = foo;
+`,
+		`
+class Foo {
+	constructor(public foo: number) {
+		this.foo += foo;
+		this.foo -= foo;
+		this.foo *= foo;
+		this.foo /= foo;
+		this.foo %= foo;
+		this.foo **= foo;
+	}
+}
+`,
+		`
+class Foo {
+	constructor(public foo: number) {
+		this.foo += 1;
+		this.foo = foo;
+	}
+}
+`,
+
+		`
+declare const name: string;
+class Foo {
+	constructor(public foo: number) {
+		this[name] = foo;
+	}
+}
+`,
+		`
+class MyClass {
+	constructor(value: string) {
+		this.value = value;
+	}
+}
+`,
+		`
+class Service {
+	update(data: object) {
+		this.data = data;
+	}
 }
 `,
 		`
@@ -59,6 +412,11 @@ class MyClass {
 	constructor(value: string) {
 		this.prop = value;
 	}
+}
+`,
+		`
+function fn(x: number) {
+	this.x = x;
 }
 `,
 	],
--- packages/ts/src/rules/parameterPropertyAssignment.ts
+++ packages/ts/src/rules/parameterPropertyAssignment.ts
@@ -6,100 +6,235 @@ import ts from "typescript";
 
 import { ruleCreator } from "./ruleCreator.ts";
 
+const COMPOUND_OPERATORS = new Set([
+	ts.SyntaxKind.AsteriskAsteriskEqualsToken,
+	ts.SyntaxKind.AsteriskEqualsToken,
+	ts.SyntaxKind.MinusEqualsToken,
+	ts.SyntaxKind.PercentEqualsToken,
+	ts.SyntaxKind.PlusEqualsToken,
+	ts.SyntaxKind.SlashEqualsToken,
+]);
+
+const UNNECESSARY_OPERATORS = new Set([
+	ts.SyntaxKind.AmpersandAmpersandEqualsToken,
+	ts.SyntaxKind.BarBarEqualsToken,
+	ts.SyntaxKind.EqualsToken,
+	ts.SyntaxKind.QuestionQuestionEqualsToken,
+]);
+
+function getIdentifierName(expr: ts.Expression): string | undefined {
+	let cur: ts.Expression = expr;
+	while (true) {
+		if (ts.isParenthesizedExpression(cur)) {
+			cur = cur.expression;
+			continue;
+		}
+		if (ts.isNonNullExpression(cur)) {
+			cur = cur.expression;
+			continue;
+		}
+		if (ts.isAsExpression(cur)) {
+			cur = cur.expression;
+			continue;
+		}
+		break;
+	}
+	return ts.isIdentifier(cur) ? cur.text : undefined;
+}
+
+function getNearestFunctionLike(
+	node: ts.Node,
+): ts.FunctionLikeDeclaration | undefined {
+	let cur = node.parent as ts.Node | undefined;
+	while (cur) {
+		if (
+			ts.isArrowFunction(cur) ||
+			ts.isFunctionDeclaration(cur) ||
+			ts.isFunctionExpression(cur) ||
+			ts.isMethodDeclaration(cur) ||
+			ts.isConstructorDeclaration(cur) ||
+			ts.isGetAccessorDeclaration(cur) ||
+			ts.isSetAccessorDeclaration(cur)
+		) {
+			return cur;
+		}
+		cur = cur.parent as ts.Node | undefined;
+	}
+	return undefined;
+}
+
+function getParameterPropertyNames(
+	ctor: ts.ConstructorDeclaration,
+): Set<string> {
+	const names = new Set<string>();
+	for (const param of ctor.parameters) {
+		if (ts.isIdentifier(param.name) && isParameterProperty(param)) {
+			names.add(param.name.text);
+		}
+	}
+	return names;
+}
+
+function getThisPropertyName(expr: ts.Expression): string | undefined {
+	if (ts.isPropertyAccessExpression(expr)) {
+		if (expr.expression.kind !== ts.SyntaxKind.ThisKeyword) {
+			return undefined;
+		}
+		return ts.isIdentifier(expr.name) ? expr.name.text : undefined;
+	}
+
+	if (ts.isElementAccessExpression(expr)) {
+		if (expr.expression.kind !== ts.SyntaxKind.ThisKeyword) {
+			return undefined;
+		}
+		const arg = expr.argumentExpression;
+		if (ts.isStringLiteral(arg)) {
+			return arg.text;
+		}
+		if (ts.isNoSubstitutionTemplateLiteral(arg)) {
+			return arg.text;
+		}
+		return undefined;
+	}
+
+	return undefined;
+}
+
+function hasPriorCompoundWrite(
+	ctor: ts.ConstructorDeclaration,
+	targetName: string,
+	beforeNode: ts.Node,
+): boolean {
+	if (!ctor.body) {
+		return false;
+	}
+
+	const statements = ctor.body.statements;
+	const beforeStmtIndex = statements.findIndex(
+		(s) => s.pos <= beforeNode.pos && s.end >= beforeNode.end,
+	);
+	if (beforeStmtIndex <= 0) {
+		return false;
+	}
+
+	const hasCompoundWrite = (n: ts.Node, root: ts.Node): boolean => {
+		if (n !== root && (ts.isFunctionLike(n) || ts.isClassLike(n))) {
+			return false;
+		}
+
+		if (ts.isBinaryExpression(n)) {
+			const lhs = getThisPropertyName(n.left);
+			if (lhs === targetName && COMPOUND_OPERATORS.has(n.operatorToken.kind)) {
+				return true;
+			}
+		}
+
+		return (
+			ts.forEachChild(n, (child) => hasCompoundWrite(child, root)) ?? false
+		);
+	};
+
+	for (let i = 0; i < beforeStmtIndex; i++) {
+		const stmt = statements[i];
+		if (!stmt) {
+			continue;
+		}
+		if (hasCompoundWrite(stmt, stmt)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function isIIFE(node: ts.FunctionLikeDeclaration): boolean {
+	if (!ts.isArrowFunction(node) && !ts.isFunctionExpression(node)) {
+		return false;
+	}
+	let parent: ts.Node = node.parent;
+	while (ts.isParenthesizedExpression(parent)) {
+		parent = parent.parent;
+	}
+	return ts.isCallExpression(parent);
+}
+
+function isParameterProperty(param: ts.ParameterDeclaration): boolean {
+	const mods = ts.getModifiers(param);
+	if (!mods) {
+		return false;
+	}
+	return mods.some(
+		(m) =>
+			m.kind === ts.SyntaxKind.PrivateKeyword ||
+			m.kind === ts.SyntaxKind.ProtectedKeyword ||
+			m.kind === ts.SyntaxKind.PublicKeyword ||
+			m.kind === ts.SyntaxKind.ReadonlyKeyword,
+	);
+}
+
 export default ruleCreator.createRule(typescriptLanguage, {
 	about: {
 		description:
-			"Disallow unnecessary property assignments in function parameters.",
+			"Disallow unnecessary assignment of constructor parameter property.",
 		id: "parameterPropertyAssignment",
 		presets: ["logical"],
 	},
 	messages: {
 		unnecessaryParameterPropertyAssignment: {
-			primary: "Parameter property assignment is unnecessary.",
+			primary:
+				"This assignment is unnecessary since it is already assigned by a parameter property.",
 			secondary: [
 				"Assigning a parameter property to itself is redundant.",
-				"Remove the unnecessary assignment.",
+				"The parameter property syntax already assigns the value to the class instance.",
 			],
+			suggestions: ["Remove the unnecessary assignment."],
 		},
 	},
 	setup(context) {
-		const isFunction = (
-			node: ts.Node,
-		): node is
-			| ts.ArrowFunction
-			| ts.ConstructorDeclaration
-			| ts.FunctionDeclaration
-			| ts.FunctionExpression
-			| ts.MethodDeclaration =>
-			ts.isFunctionDeclaration(node) ||
-			ts.isFunctionExpression(node) ||
-			ts.isArrowFunction(node) ||
-			ts.isMethodDeclaration(node) ||
-			ts.isConstructorDeclaration(node);
-
-		const hasParameter = (
-			func:
-				| ts.ArrowFunction
-				| ts.ConstructorDeclaration
-				| ts.FunctionDeclaration
-				| ts.FunctionExpression
-				| ts.MethodDeclaration,
-			paramName: string,
-		): boolean => {
-			for (const param of func.parameters) {
-				if (
-					param.name.kind === ts.SyntaxKind.Identifier &&
-					param.name.text === paramName
-				) {
-					return true;
-				}
-			}
-			return false;
-		};
-
 		return {
 			visitors: {
 				BinaryExpression: (node, { sourceFile }) => {
-					// Check for pattern: this.x = x (where x is a parameter)
-					if (node.operatorToken.kind !== ts.SyntaxKind.EqualsToken) {
+					if (!UNNECESSARY_OPERATORS.has(node.operatorToken.kind)) {
 						return;
 					}
 
-					if (
-						!ts.isPropertyAccessExpression(node.left) ||
-						node.left.expression.kind !== ts.SyntaxKind.ThisKeyword ||
-						!ts.isIdentifier(node.left.name) ||
-						!ts.isIdentifier(node.right)
-					) {
+					const lhsName = getThisPropertyName(node.left);
+					if (lhsName === undefined) {
 						return;
 					}
 
-					const propertyName = node.left.name.text;
-					const parameterName = node.right.text;
-
-					if (propertyName !== parameterName) {
+					const rhsName = getIdentifierName(node.right);
+					if (rhsName === undefined) {
 						return;
 					}
 
-					// Walk up the parent chain to find the containing function
-					let current: ts.Node | undefined = node;
-					// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-					while (current !== undefined) {
-						if (isFunction(current) && hasParameter(current, parameterName)) {
-							// Only report for constructors and methods (not regular functions)
-							if (
-								ts.isConstructorDeclaration(current) ||
-								ts.isMethodDeclaration(current)
-							) {
-								context.report({
-									message: "unnecessaryParameterPropertyAssignment",
-									range: getTSNodeRange(node, sourceFile),
-								});
-							}
-							return;
-						}
-						current = current.parent;
+					if (lhsName !== rhsName) {
+						return;
 					}
+
+					let functionNode = getNearestFunctionLike(node);
+					while (functionNode && isIIFE(functionNode)) {
+						functionNode = getNearestFunctionLike(functionNode);
+					}
+
+					if (!functionNode || !ts.isConstructorDeclaration(functionNode)) {
+						return;
+					}
+
+					const parameterPropertyNames =
+						getParameterPropertyNames(functionNode);
+					if (!parameterPropertyNames.has(rhsName)) {
+						return;
+					}
+
+					if (hasPriorCompoundWrite(functionNode, rhsName, node)) {
+						return;
+					}
+
+					context.report({
+						message: "unnecessaryParameterPropertyAssignment",
+						range: getTSNodeRange(node, sourceFile),
+					});
 				},
 			},
 		};
