bf9d6b6e9d1c7e23b49d1c91396a048fc2114318
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-26 13:04:44 -0500

    progress: only 98/31 to go

--- packages/core/src/plugins/createPlugin.ts
+++ packages/core/src/plugins/createPlugin.ts
@@ -31,19 +31,17 @@ export interface CreatePluginOptionsWithoutFiles<
 	rules: Rules;
 }
 
-// ...
-
 export type UnsafeAnyRule<About extends RuleAbout = RuleAbout> = Rule<
 	About,
 	// TODO: How to make these types work with createPlugin.test.ts & co.?
-	/* flint-disable-lines-begin explicitAnys */
+	// flint-disable-lines-begin explicitAnys
 	/* eslint-disable @typescript-eslint/no-explicit-any */
 	any,
 	any,
 	any,
 	any
 	/* eslint-enable @typescript-eslint/no-explicit-any */
-	/* flint-disable-lines-end explicitAnys */
+	// flint-disable-lines-end explicitAnys
 >;
 
 export function createPlugin<
