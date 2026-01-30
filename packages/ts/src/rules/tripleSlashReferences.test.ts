2805d1842ead5494e94e0470db378781e73dce0c
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-30 12:04:13 -0500

    tests touchups

--- packages/ts/src/rules/tripleSlashReferences.test.ts
+++ packages/ts/src/rules/tripleSlashReferences.test.ts
@@ -6,32 +6,38 @@ ruleTester.describe(rule, {
 		{
 			code: `
 /// <reference path="./types.d.ts" />
-const value = 1;`,
+const value = 1;
+`,
 			snapshot: `
 /// <reference path="./types.d.ts" />
                      ~~~~~~~~~~~~
                      Prefer ECMAScript modules and/or TSConfig settings over legacy triple-slash directives.
-const value = 1;`,
+const value = 1;
+`,
 		},
 		{
 			code: `
 /// <reference types="node" />
-const process = {};`,
+const process = {};
+`,
 			snapshot: `
 /// <reference types="node" />
                       ~~~~
                       Prefer ECMAScript modules and/or TSConfig settings over legacy triple-slash directives.
-const process = {};`,
+const process = {};
+`,
 		},
 		{
 			code: `
 /// <reference lib="es2020" />
-const value = 1;`,
+const value = 1;
+`,
 			snapshot: `
 /// <reference lib="es2020" />
                     ~~~~~~
                     Prefer ECMAScript modules and/or TSConfig settings over legacy triple-slash directives.
-const value = 1;`,
+const value = 1;
+`,
 		},
 	],
 	valid: [
