65f195280d1f46e83864ea06190d7895e89be025
Josh Goldberg <git@joshuakgoldberg.com>
2026-02-01 10:19:19 -0500

    touchups

--- packages/ts/src/rules/returnThisTypes.test.ts
+++ packages/ts/src/rules/returnThisTypes.test.ts
@@ -5,24 +5,24 @@ ruleTester.describe(rule, {
 	invalid: [
 		{
 			code: `
-class Builder {
-    setValue(): Builder {
+class Foo {
+    f(): Foo {
         return this;
     }
 }
 `,
 			output: `
-class Builder {
-    setValue(): this {
+class Foo {
+    f(): this {
         return this;
     }
 }
 `,
 			snapshot: `
-class Builder {
-    setValue(): Builder {
-                ~~~~~~~
-                Use \`this\` as the return type instead of the class name for polymorphic chaining.
+class Foo {
+    f(): Foo {
+         ~~~
+         Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
         return this;
     }
 }
@@ -30,95 +30,51 @@ class Builder {
 		},
 		{
 			code: `
-class Builder {
-    value = function(): Builder {
+class Foo {
+    f = function (): Foo {
         return this;
-    }
+    };
 }
 `,
 			output: `
-class Builder {
-    value = function(): this {
+class Foo {
+    f = function (): this {
         return this;
-    }
+    };
 }
 `,
 			snapshot: `
-class Builder {
-    value = function(): Builder {
-                        ~~~~~~~
-                        Use \`this\` as the return type instead of the class name for polymorphic chaining.
+class Foo {
+    f = function (): Foo {
+                     ~~~
+                     Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
         return this;
-    }
+    };
 }
 `,
 		},
 		{
 			code: `
-class Builder {
-    setValue = (): Builder => this
-}
-`,
-			output: `
-class Builder {
-    setValue = (): this => this
-}
-`,
-			snapshot: `
-class Builder {
-    setValue = (): Builder => this
-                   ~~~~~~~
-                   Use \`this\` as the return type instead of the class name for polymorphic chaining.
-}
-`,
-		},
-		{
-			code: `
-class Builder {
-    setValue = (): Builder => {
-        return this;
-    }
-}
-`,
-			output: `
-class Builder {
-    setValue = (): this => {
-        return this;
-    }
-}
-`,
-			snapshot: `
-class Builder {
-    setValue = (): Builder => {
-                   ~~~~~~~
-                   Use \`this\` as the return type instead of the class name for polymorphic chaining.
-        return this;
-    }
-}
-`,
-		},
-		{
-			code: `
-class Builder {
-    setValue(): Builder {
+class Foo {
+    f(): Foo {
         const self = this;
         return self;
     }
 }
 `,
 			output: `
-class Builder {
-    setValue(): this {
+class Foo {
+    f(): this {
         const self = this;
         return self;
     }
 }
 `,
 			snapshot: `
-class Builder {
-    setValue(): Builder {
-                ~~~~~~~
-                Use \`this\` as the return type instead of the class name for polymorphic chaining.
+class Foo {
+    f(): Foo {
+         ~~~
+         Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
         const self = this;
         return self;
     }
@@ -127,25 +83,221 @@ class Builder {
 		},
 		{
 			code: `
-class Builder {
-    setValue(): Builder | undefined {
+class Foo {
+    f = (): Foo => {
+        return this;
+    };
+}
+`,
+			output: `
+class Foo {
+    f = (): this => {
+        return this;
+    };
+}
+`,
+			snapshot: `
+class Foo {
+    f = (): Foo => {
+            ~~~
+            Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        return this;
+    };
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    f = (): Foo => {
+        const self = this;
+        return self;
+    };
+}
+`,
+			output: `
+class Foo {
+    f = (): this => {
+        const self = this;
+        return self;
+    };
+}
+`,
+			snapshot: `
+class Foo {
+    f = (): Foo => {
+            ~~~
+            Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        const self = this;
+        return self;
+    };
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    f = (): Foo => this;
+}
+`,
+			output: `
+class Foo {
+    f = (): this => this;
+}
+`,
+			snapshot: `
+class Foo {
+    f = (): Foo => this;
+            ~~~
+            Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    accessor f = (): Foo => {
+        return this;
+    };
+}
+`,
+			output: `
+class Foo {
+    accessor f = (): this => {
+        return this;
+    };
+}
+`,
+			snapshot: `
+class Foo {
+    accessor f = (): Foo => {
+                     ~~~
+                     Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        return this;
+    };
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    accessor f = (): Foo => this;
+}
+`,
+			output: `
+class Foo {
+    accessor f = (): this => this;
+}
+`,
+			snapshot: `
+class Foo {
+    accessor f = (): Foo => this;
+                     ~~~
+                     Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    f1(): Foo | undefined {
+        return this;
+    }
+    f2(): this | undefined {
         return this;
     }
 }
 `,
 			output: `
-class Builder {
-    setValue(): this | undefined {
+class Foo {
+    f1(): this | undefined {
+        return this;
+    }
+    f2(): this | undefined {
         return this;
     }
 }
 `,
 			snapshot: `
-class Builder {
-    setValue(): Builder | undefined {
-                ~~~~~~~
-                Use \`this\` as the return type instead of the class name for polymorphic chaining.
+class Foo {
+    f1(): Foo | undefined {
+          ~~~
+          Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
         return this;
+    }
+    f2(): this | undefined {
+        return this;
+    }
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    bar(): Foo | undefined {
+        if (Math.random() > 0.5) {
+            return this;
+        }
+    }
+}
+`,
+			output: `
+class Foo {
+    bar(): this | undefined {
+        if (Math.random() > 0.5) {
+            return this;
+        }
+    }
+}
+`,
+			snapshot: `
+class Foo {
+    bar(): Foo | undefined {
+           ~~~
+           Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        if (Math.random() > 0.5) {
+            return this;
+        }
+    }
+}
+`,
+		},
+		{
+			code: `
+class Foo {
+    bar(num: 1 | 2): Foo {
+        switch (num) {
+            case 1:
+                return this;
+            case 2:
+                return this;
+        }
+    }
+}
+`,
+			output: `
+class Foo {
+    bar(num: 1 | 2): this {
+        switch (num) {
+            case 1:
+                return this;
+            case 2:
+                return this;
+        }
+    }
+}
+`,
+			snapshot: `
+class Foo {
+    bar(num: 1 | 2): Foo {
+                     ~~~
+                     Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        switch (num) {
+            case 1:
+                return this;
+            case 2:
+                return this;
+        }
     }
 }
 `,
@@ -154,6 +306,7 @@ class Builder {
 			code: `
 class Animal<T> {
     eat(): Animal<T> {
+        console.log("I'm moving!");
         return this;
     }
 }
@@ -161,6 +314,7 @@ class Animal<T> {
 			output: `
 class Animal<T> {
     eat(): this {
+        console.log("I'm moving!");
         return this;
     }
 }
@@ -169,7 +323,8 @@ class Animal<T> {
 class Animal<T> {
     eat(): Animal<T> {
            ~~~~~~~~~
-           Use \`this\` as the return type instead of the class name for polymorphic chaining.
+           Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        console.log("I'm moving!");
         return this;
     }
 }
@@ -177,77 +332,145 @@ class Animal<T> {
 		},
 		{
 			code: `
-class Builder {
-    get self(): Builder {
-        return this;
+declare const valueUnion: number | string;
+
+class BaseUnion {
+    f(): BaseUnion | string {
+        if (Math.random()) {
+            return this;
+        }
+
+        return valueUnion;
     }
 }
 `,
 			output: `
-class Builder {
-    get self(): this {
-        return this;
+declare const valueUnion: number | string;
+
+class BaseUnion {
+    f(): this | string {
+        if (Math.random()) {
+            return this;
+        }
+
+        return valueUnion;
     }
 }
 `,
 			snapshot: `
-class Builder {
-    get self(): Builder {
-                ~~~~~~~
-                Use \`this\` as the return type instead of the class name for polymorphic chaining.
-        return this;
-    }
-}
-`,
-		},
-		{
-			code: `
-class Builder {
-    setValue(): Builder {
-        if (Math.random() > 0.5) {
+declare const valueUnion: number | string;
+
+class BaseUnion {
+    f(): BaseUnion | string {
+         ~~~~~~~~~
+         Prefer \`this\` as the return type instead of the class name for polymorphic chaining.
+        if (Math.random()) {
             return this;
         }
-        return this;
-    }
-}
-`,
-			output: `
-class Builder {
-    setValue(): this {
-        if (Math.random() > 0.5) {
-            return this;
-        }
-        return this;
-    }
-}
-`,
-			snapshot: `
-class Builder {
-    setValue(): Builder {
-                ~~~~~~~
-                Use \`this\` as the return type instead of the class name for polymorphic chaining.
-        if (Math.random() > 0.5) {
-            return this;
-        }
-        return this;
+
+        return valueUnion;
     }
 }
 `,
 		},
 	],
 	valid: [
-		`class Builder { setValue() { return this; } }`,
-		`class Builder { setValue(): this { return this; } }`,
-		`class Builder { setValue(): Builder { return new Builder(); } }`,
-		`class Builder { setValue(): any { return this; } }`,
-		`class Builder { setValue(this: Builder): Builder { return this; } }`,
-		`class Builder { setValue(value: Builder): Builder { return value; } }`,
-		`class Derived extends Base { setValue(): Base { return this; } }`,
-		`class Builder { static create(): Builder { return new Builder(); } }`,
-		`class Builder { setValue(): Builder { } }`,
-		`class Builder { setValue(): string { return ""; } }`,
-		`class Builder { setValue(): void { } }`,
-		`class Builder { private getValue = (): Builder => new Builder() }`,
-		`class Builder { setValue(): Builder { return someOtherBuilder; } }`,
+		`
+class Foo {
+    f1() {}
+    f2(): Foo {
+        return new Foo();
+    }
+    f3() {
+        return this;
+    }
+    f4(): this {
+        return this;
+    }
+    f5(): any {
+        return this;
+    }
+    f6(): unknown {
+        return this;
+    }
+    f7(foo: Foo): Foo {
+        return Math.random() > 0.5 ? foo : this;
+    }
+    f10(this: Foo, that: Foo): Foo;
+    f11(): Foo {
+        return;
+    }
+    f13(this: Foo): Foo {
+        return this;
+    }
+    f14(): { f14: Function } {
+        return this;
+    }
+    f15(): Foo | this {
+        return Math.random() > 0.5 ? new Foo() : this;
+    }
+}
+`,
+		`
+class Foo {
+    f1 = () => {};
+    f2 = (): Foo => {
+        return new Foo();
+    };
+    f3 = () => this;
+    f4 = (): this => {
+        return this;
+    };
+    f5 = (): Foo => new Foo();
+    f6 = '';
+}
+`,
+		`
+const Foo = class {
+    bar() {
+        return this;
+    }
+};
+`,
+		`
+class Base {}
+class Derived extends Base {
+    f(): Base {
+        return this;
+    }
+}
+`,
+		`
+class Foo {
+    accessor f = () => {
+        return this;
+    };
+}
+`,
+		`
+class Foo {
+    accessor f = (): this => {
+        return this;
+    };
+}
+`,
+		`
+class Foo {
+    f?: string;
+}
+`,
+		`
+declare const valueUnion: BaseUnion | string;
+
+class BaseUnion {
+    f(): BaseUnion | string {
+        if (Math.random()) {
+            return this;
+        }
+
+        return valueUnion;
+    }
+}
+`,
 	],
 });
--- packages/ts/src/rules/returnThisTypes.ts
+++ packages/ts/src/rules/returnThisTypes.ts
@@ -1,5 +1,6 @@
 import {
 	type AST,
+	getTSNodeRange,
 	type TypeScriptFileServices,
 	typescriptLanguage,
 } from "@flint.fyi/typescript-language";
@@ -7,392 +8,191 @@ import * as tsutils from "ts-api-utils";
 import * as ts from "typescript";
 
 import { ruleCreator } from "./ruleCreator.ts";
+import { forEachReturnStatement } from "./utils/forEachReturnStatement.ts";
 
-type FunctionLikeBody =
+type ClassLikeDeclaration = AST.ClassDeclaration | AST.ClassExpression;
+
+type FunctionLike =
 	| AST.ArrowFunction
 	| AST.FunctionExpression
 	| AST.GetAccessorDeclaration
 	| AST.MethodDeclaration;
 
-function allReturnsAreThis(body: ts.Node, aliases: Set<string>): boolean {
-	const returnStatements: ts.ReturnStatement[] = [];
-
-	function collectReturns(node: ts.Node): void {
-		if (tsutils.isFunctionScopeBoundary(node)) {
-			return;
-		}
-
-		if (ts.isReturnStatement(node)) {
-			returnStatements.push(node);
-			return;
-		}
-
-		ts.forEachChild(node, collectReturns);
-	}
-
-	ts.forEachChild(body, collectReturns);
-
-	if (returnStatements.length === 0) {
-		return false;
-	}
-
-	let hasThisReturn = false;
-	for (const returnStmt of returnStatements) {
-		if (!returnStmt.expression) {
-			continue;
-		}
-		if (isThisOrAlias(returnStmt.expression, aliases)) {
-			hasThisReturn = true;
-		} else {
-			return false;
-		}
-	}
-
-	return hasThisReturn;
-}
-
-function collectThisAliases(body: ts.Node): Set<string> {
-	const aliases = new Set<string>();
-
-	function visit(node: ts.Node): void {
-		if (
-			ts.isVariableDeclaration(node) &&
-			ts.isIdentifier(node.name) &&
-			node.initializer?.kind === ts.SyntaxKind.ThisKeyword
-		) {
-			aliases.add(node.name.text);
-		}
-		ts.forEachChild(node, visit);
-	}
-
-	ts.forEachChild(body, visit);
-	return aliases;
-}
-
-function findClassTypeReferenceNode(
-	typeNode: ts.TypeNode,
-	className: string,
-	typeChecker: ts.TypeChecker,
-	classSymbol: ts.Symbol | undefined,
-): ts.TypeReferenceNode | undefined {
-	if (ts.isTypeReferenceNode(typeNode)) {
-		const typeName = typeNode.typeName;
-		if (ts.isIdentifier(typeName) && typeName.text === className) {
-			const symbol = typeChecker.getSymbolAtLocation(typeName);
-			if (symbol && classSymbol) {
-				const resolvedSymbol =
-					symbol.flags & ts.SymbolFlags.Alias
-						? typeChecker.getAliasedSymbol(symbol)
-						: symbol;
-				const resolvedClassSymbol =
-					classSymbol.flags & ts.SymbolFlags.Alias
-						? typeChecker.getAliasedSymbol(classSymbol)
-						: classSymbol;
-				if (resolvedSymbol === resolvedClassSymbol) {
-					return typeNode;
-				}
-			} else {
-				return typeNode;
-			}
-		}
-	}
-
-	if (ts.isUnionTypeNode(typeNode)) {
-		for (const type of typeNode.types) {
-			const found = findClassTypeReferenceNode(
-				type,
-				className,
-				typeChecker,
-				classSymbol,
-			);
-			if (found) {
-				return found;
-			}
-		}
-	}
-
-	return undefined;
-}
-
-function getClassName(
-	classNode: AST.ClassDeclaration | AST.ClassExpression,
-): string | undefined {
-	return classNode.name?.text;
-}
-
-function getEnclosingClass(
-	node: ts.Node,
-): AST.ClassDeclaration | AST.ClassExpression | undefined {
-	let current = node.parent as ts.Node | undefined;
-	while (current) {
-		if (ts.isClassDeclaration(current) || ts.isClassExpression(current)) {
-			return current as AST.ClassDeclaration | AST.ClassExpression;
-		}
-		current = current.parent;
-	}
-	return undefined;
-}
-
-function hasExplicitThisParameter(node: FunctionLikeBody): boolean {
-	const firstParam = node.parameters[0];
-	if (!firstParam) {
-		return false;
-	}
-	return ts.isIdentifier(firstParam.name) && firstParam.name.text === "this";
-}
-
-function hasModifier(
-	modifiers: ts.NodeArray<AST.ModifierLike> | undefined,
-	kind: ts.SyntaxKind,
-): boolean {
-	return modifiers?.some((modifier) => modifier.kind === kind) ?? false;
-}
-
-function isArrowReturningThis(
-	arrow: AST.ArrowFunction,
-	aliases: Set<string>,
-): boolean {
-	if (!ts.isBlock(arrow.body)) {
-		return isThisOrAlias(arrow.body, aliases);
-	}
-	return allReturnsAreThis(arrow.body, aliases);
-}
-
-function isThisOrAlias(
-	expression: ts.Expression,
-	aliases: Set<string>,
-): boolean {
-	if (expression.kind === ts.SyntaxKind.ThisKeyword) {
-		return true;
-	}
-	if (ts.isIdentifier(expression) && aliases.has(expression.text)) {
-		return true;
-	}
-	return false;
-}
-
-function typeReferencesClass(
-	typeNode: ts.TypeNode,
-	className: string,
-	typeChecker: ts.TypeChecker,
-	classSymbol: ts.Symbol | undefined,
-): boolean {
-	if (ts.isTypeReferenceNode(typeNode)) {
-		const typeName = typeNode.typeName;
-		if (ts.isIdentifier(typeName) && typeName.text === className) {
-			const symbol = typeChecker.getSymbolAtLocation(typeName);
-			if (symbol && classSymbol) {
-				const resolvedSymbol =
-					symbol.flags & ts.SymbolFlags.Alias
-						? typeChecker.getAliasedSymbol(symbol)
-						: symbol;
-				const resolvedClassSymbol =
-					classSymbol.flags & ts.SymbolFlags.Alias
-						? typeChecker.getAliasedSymbol(classSymbol)
-						: classSymbol;
-				return resolvedSymbol === resolvedClassSymbol;
-			}
-			return true;
-		}
-	}
-
-	if (ts.isUnionTypeNode(typeNode)) {
-		return typeNode.types.some((type) =>
-			typeReferencesClass(type, className, typeChecker, classSymbol),
-		);
-	}
-
-	return false;
-}
-
 export default ruleCreator.createRule(typescriptLanguage, {
 	about: {
 		description:
-			"Prefer `this` return type over explicit class name when returning `this`.",
+			"Enforce that `this` is used when only `this` type is returned.",
 		id: "returnThisTypes",
 		presets: ["logical"],
 	},
 	messages: {
-		preferThisReturnType: {
+		preferThis: {
 			primary:
-				"Use `this` as the return type instead of the class name for polymorphic chaining.",
+				"Prefer `this` as the return type instead of the class name for polymorphic chaining.",
 			secondary: [
+				"Annotating this method as returning the class extends to sub-classes as well.",
+				"Any sub-class will be implicitly typed as returning this parent class, rather than itself.",
 				"Using `this` preserves method chaining behavior in subclasses.",
 			],
 			suggestions: ["Replace the class name with `this`."],
 		},
 	},
 	setup(context) {
-		function checkMember(
-			node: AST.GetAccessorDeclaration | AST.MethodDeclaration,
+		function checkFunction(
+			functionNode: FunctionLike,
+			originalClassNode: AST.AnyNode,
 			{ sourceFile, typeChecker }: TypeScriptFileServices,
 		) {
-			if (hasModifier(node.modifiers, ts.SyntaxKind.StaticKeyword)) {
-				return;
-			}
-
-			if (!node.type || !node.body) {
-				return;
-			}
-
-			if (node.type.kind === ts.SyntaxKind.ThisType) {
-				return;
-			}
-
-			if (hasExplicitThisParameter(node)) {
-				return;
-			}
-
-			const classNode = getEnclosingClass(node);
-			if (!classNode) {
-				return;
-			}
-
-			const className = getClassName(classNode);
-			if (!className) {
-				return;
-			}
-
-			const classSymbol = classNode.name
-				? typeChecker.getSymbolAtLocation(classNode.name)
-				: undefined;
-
 			if (
-				!typeReferencesClass(node.type, className, typeChecker, classSymbol)
+				originalClassNode.kind !== ts.SyntaxKind.ClassDeclaration &&
+				originalClassNode.kind !== ts.SyntaxKind.ClassExpression
 			) {
 				return;
 			}
 
-			const aliases = collectThisAliases(node.body);
-			if (!allReturnsAreThis(node.body, aliases)) {
+			const className = originalClassNode.name?.text;
+			if (!className || !functionNode.type) {
 				return;
 			}
 
-			const typeReferenceNode = findClassTypeReferenceNode(
-				node.type,
-				className,
-				typeChecker,
-				classSymbol,
-			);
-			if (!typeReferenceNode) {
+			const nameNode = tryGetNameInType(className, functionNode.type);
+			if (
+				!nameNode ||
+				!isFunctionReturningThis(functionNode, originalClassNode, typeChecker)
+			) {
 				return;
 			}
+
+			const range = getTSNodeRange(nameNode, sourceFile);
 
 			context.report({
 				fix: {
-					range: {
-						begin: typeReferenceNode.getStart(sourceFile),
-						end: typeReferenceNode.getEnd(),
-					},
+					range,
 					text: "this",
 				},
-				message: "preferThisReturnType",
-				range: {
-					begin: typeReferenceNode.getStart(sourceFile),
-					end: typeReferenceNode.getEnd(),
-				},
+				message: "preferThis",
+				range,
 			});
 		}
 
-		function checkPropertyFunction(
-			node: AST.PropertyDeclaration,
-			services: TypeScriptFileServices,
+		function isFunctionReturningThis(
+			functionNode: FunctionLike,
+			originalClassNode: ClassLikeDeclaration,
+			typeChecker: ts.TypeChecker,
 		) {
-			const { sourceFile, typeChecker } = services;
-
-			if (hasModifier(node.modifiers, ts.SyntaxKind.StaticKeyword)) {
-				return;
+			if (!functionNode.body || isThisSpecifiedInParameters(functionNode)) {
+				return false;
 			}
 
-			if (!node.initializer) {
-				return;
+			const classType = typeChecker.getTypeAtLocation(
+				originalClassNode,
+			) as ts.InterfaceType;
+
+			if (functionNode.body.kind !== ts.SyntaxKind.Block) {
+				return (
+					classType.thisType ===
+					typeChecker.getTypeAtLocation(functionNode.body)
+				);
 			}
 
-			if (
-				!ts.isFunctionExpression(node.initializer) &&
-				!ts.isArrowFunction(node.initializer)
-			) {
-				return;
-			}
+			let hasReturnThis = false;
+			let hasReturnClassType = false;
 
-			const func = node.initializer;
-			const funcType = func.type;
-
-			if (!funcType) {
-				return;
-			}
-
-			if (funcType.kind === ts.SyntaxKind.ThisType) {
-				return;
-			}
-
-			if (hasExplicitThisParameter(func)) {
-				return;
-			}
-
-			const classNode = getEnclosingClass(node);
-			if (!classNode) {
-				return;
-			}
-
-			const className = getClassName(classNode);
-			if (!className) {
-				return;
-			}
-
-			const classSymbol = classNode.name
-				? typeChecker.getSymbolAtLocation(classNode.name)
-				: undefined;
-
-			if (!typeReferencesClass(funcType, className, typeChecker, classSymbol)) {
-				return;
-			}
-
-			const { body } = func;
-
-			const aliases = ts.isBlock(body)
-				? collectThisAliases(body)
-				: new Set<string>();
-
-			if (ts.isArrowFunction(func)) {
-				if (!isArrowReturningThis(func, aliases)) {
+			forEachReturnStatement(functionNode.body, (statement) => {
+				if (!statement.expression) {
 					return;
 				}
-			} else if (!allReturnsAreThis(body, aliases)) {
-				return;
-			}
 
-			const typeReferenceNode = findClassTypeReferenceNode(
-				funcType,
-				className,
-				typeChecker,
-				classSymbol,
-			);
-			if (!typeReferenceNode) {
-				return;
-			}
+				if (statement.expression.kind === ts.SyntaxKind.ThisKeyword) {
+					hasReturnThis = true;
+					return;
+				}
 
-			context.report({
-				fix: {
-					range: {
-						begin: typeReferenceNode.getStart(sourceFile),
-						end: typeReferenceNode.getEnd(),
-					},
-					text: "this",
-				},
-				message: "preferThisReturnType",
-				range: {
-					begin: typeReferenceNode.getStart(sourceFile),
-					end: typeReferenceNode.getEnd(),
-				},
+				const type = typeChecker.getTypeAtLocation(statement.expression);
+				if (classType === type) {
+					hasReturnClassType = true;
+					return true;
+				}
+
+				if (classType.thisType === type) {
+					hasReturnThis = true;
+					return;
+				}
+
+				if (
+					tsutils.isUnionType(type) &&
+					type.types.some((constituent) => constituent === classType)
+				) {
+					hasReturnClassType = true;
+					return true;
+				}
 			});
+
+			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+			return !hasReturnClassType && hasReturnThis;
+		}
+
+		function isThisSpecifiedInParameters(functionNode: FunctionLike) {
+			if (!functionNode.parameters.length) {
+				return false;
+			}
+
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			const firstArgument = functionNode.parameters[0]!;
+
+			return (
+				ts.isIdentifier(firstArgument.name) &&
+				firstArgument.name.text === "this"
+			);
+		}
+
+		function tryGetNameInType(
+			name: string,
+			typeNode: ts.TypeNode,
+		): ts.TypeReferenceNode | undefined {
+			if (
+				ts.isTypeReferenceNode(typeNode) &&
+				ts.isIdentifier(typeNode.typeName) &&
+				typeNode.typeName.text === name
+			) {
+				return typeNode;
+			}
+
+			if (ts.isUnionTypeNode(typeNode)) {
+				for (const type of typeNode.types) {
+					const found = tryGetNameInType(name, type);
+					if (found) {
+						return found;
+					}
+				}
+			}
+
+			return undefined;
+		}
+
+		function checkMethodLike(
+			node: AST.GetAccessorDeclaration | AST.MethodDeclaration,
+			services: TypeScriptFileServices,
+		) {
+			checkFunction(node, node.parent, services);
 		}
 
 		return {
 			visitors: {
-				GetAccessor: checkMember,
-				MethodDeclaration: checkMember,
-				PropertyDeclaration: checkPropertyFunction,
+				GetAccessor: checkMethodLike,
+				MethodDeclaration: checkMethodLike,
+				PropertyDeclaration(
+					node: AST.PropertyDeclaration,
+					services: TypeScriptFileServices,
+				) {
+					if (
+						node.initializer &&
+						(ts.isFunctionExpression(node.initializer) ||
+							ts.isArrowFunction(node.initializer))
+					) {
+						checkFunction(node.initializer, node.parent, services);
+					}
+				},
 			},
 		};
 	},
