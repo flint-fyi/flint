17c7df934d1f879ae2cc4a5cbac79943fd47d5ce
Josh Goldberg <git@joshuakgoldberg.com>
2026-02-02 10:26:50 -0500

    lint

--- packages/json-language/src/language.ts
+++ packages/json-language/src/language.ts
@@ -38,6 +38,7 @@ export const jsonLanguage = createLanguage<JsonNodeVisitors, JsonFileServices>({
 			const key = ts.SyntaxKind[node.kind] as keyof typeof visitors;
 
 			// @ts-expect-error -- This should work...?
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 			visitors[key]?.(node, visitorServices);
 
 			node.forEachChild(visit);
