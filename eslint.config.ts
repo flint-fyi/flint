9a50a724e3c74269d75765a98b56a331daaea2d5
michael faith <michaelfaith@users.noreply.github.com>
2026-02-02 17:54:18 -0600

    revert eslint and ci yamls

--- eslint.config.ts
+++ eslint.config.ts
@@ -223,6 +223,10 @@ export default defineConfig(
 		files: ["**/*.{yml,yaml}"],
 		rules: {
 			"yml/file-extension": "error",
+			"yml/sort-keys": [
+				"error",
+				{ order: { type: "asc" }, pathPattern: "^.*$" },
+			],
 			"yml/sort-sequence-values": [
 				"error",
 				{ order: { type: "asc" }, pathPattern: "^.*$" },
