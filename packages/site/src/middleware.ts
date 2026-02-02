0bcb73ffa54b015d080024b0ba78283cfbe0d28e
Josh Goldberg <git@joshuakgoldberg.com>
2026-02-02 11:31:59 -0500

    debugging: warn

--- packages/site/src/middleware.ts
+++ packages/site/src/middleware.ts
@@ -1,7 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-	console.log("[Josh] Looking at url:", context.request.url);
+	console.warn("[Josh] Looking at url:", context.request.url);
 
 	if (context.request.url !== context.request.url.toLowerCase()) {
 		return Response.redirect(context.request.url.toLowerCase(), 307);
