2284b1393b91a038572297e9904d46f1707dc8ee
auvred <aauvred@gmail.com>
2026-03-18 13:33:04 +0300

    tweaks

--- packages/vue/src/plugin.ts
+++ packages/vue/src/plugin.ts
@@ -1,8 +1,12 @@
 import { createPlugin } from "@flint.fyi/core";
 
 import vForKeys from "./rules/vForKeys.ts";
+import { ts } from "@flint.fyi/ts";
 
 export const vue = createPlugin({
+	files: {
+		all: [ts.files.all, "**/*.vue"],
+	},
 	name: "Vue.js",
 	rules: [vForKeys],
 });
