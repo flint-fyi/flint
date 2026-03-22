816232d5751dd4e7951f45d08597857c32ce675b
auvred <aauvred@gmail.com>
2026-03-22 15:12:07 +0300

    add docs & fix presets

--- packages/astro/src/rules/ruleCreator.ts
+++ packages/astro/src/rules/ruleCreator.ts
@@ -3,5 +3,5 @@ import { RuleCreator } from "@flint.fyi/core";
 export const ruleCreator = new RuleCreator({
 	docs: (ruleId) => `https://flint.fyi/rules/astro/${ruleId.toLowerCase()}`,
 	pluginId: "astro",
-	presets: ["logical", "logicalStrict", "stylistic", "stylisticStrict"],
+	presets: ["logical", "stylistic", "security"],
 });
