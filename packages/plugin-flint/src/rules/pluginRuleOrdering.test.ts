59dfbfcc5e7496081379716b5b5455071ea76284
Konv Suu <2583695112@qq.com>
2026-03-26 17:39:50 +0800

    feat: add `pluginRuleOrdering` rule

---
+++ packages/plugin-flint/src/rules/pluginRuleOrdering.test.ts
@@ -0,0 +1,134 @@
+import rule from "./pluginRuleOrdering.ts";
+import { ruleTester } from "./ruleTester.ts";
+
+ruleTester.describe(rule, {
+	invalid: [
+		{
+			code: `
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+declare const gammaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		gammaRule,
+		alphaRule,
+		betaRule,
+	],
+});
+`,
+			output: `
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+declare const gammaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		alphaRule,
+		betaRule,
+		gammaRule,
+	],
+});
+`,
+			snapshot: `
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+declare const gammaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		gammaRule,
+		~~~~~~~~~
+		Flint plugin rules should be listed in alphabetical order.
+		alphaRule,
+		betaRule,
+	],
+});
+`,
+		},
+		{
+			code: `
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		betaRule, // keep this comment
+		alphaRule,
+	],
+});
+`,
+			snapshot: `
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		betaRule, // keep this comment
+		~~~~~~~~
+		Flint plugin rules should be listed in alphabetical order.
+		alphaRule,
+	],
+});
+`,
+		},
+	],
+	valid: [
+		`
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+
+makePlugin({
+	name: "Plugin",
+	rules: [
+		alphaRule,
+		betaRule,
+	],
+});
+`,
+		`
+import { createPlugin as makePlugin } from "@flint.fyi/core";
+
+declare const alphaRule: any;
+declare const betaRule: any;
+declare const gammaRule: any;
+declare const otherRules: any[];
+
+makePlugin({
+	name: "Plugin",
+	rules: [alphaRule, ...otherRules, gammaRule],
+});
+`,
+		`
+declare function createPlugin(options: {
+	name: string;
+	rules: unknown[];
+}): void;
+
+declare const alphaRule: any;
+declare const betaRule: any;
+
+createPlugin({
+	name: "Local",
+	rules: [betaRule, alphaRule],
+});
+`,
+	],
+});
