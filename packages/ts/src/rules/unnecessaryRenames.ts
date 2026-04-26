382eaca3a7cccd748da5e52500e509d81d2a3a94
Josh Goldberg <git@joshuakgoldberg.com>
2026-04-26 18:15:02 -0400

    make comprehensive and refactor

--- packages/ts/src/rules/unnecessaryRenames.ts
+++ packages/ts/src/rules/unnecessaryRenames.ts
@@ -1,17 +1,54 @@
+import type { CharacterReportRange } from "@flint.fyi/core";
 import {
 	type AST,
 	getTSNodeRange,
-	type TypeScriptFileServices,
 	typescriptLanguage,
+	unwrapParenthesizedNode,
 } from "@flint.fyi/typescript-language";
 import ts from "typescript";
 
 import { ruleCreator } from "./ruleCreator.ts";
+import { countCommentsInRange } from "./utils/countCommentsInRange.ts";
+
+// TODO: Use a util like getStaticValue
+// https://github.com/flint-fyi/flint/issues/1298
+function getModuleNameText(node: ts.Node | undefined) {
+	if (!node) {
+		return;
+	}
+
+	if (ts.isIdentifier(node) || ts.isStringLiteral(node)) {
+		return node.text;
+	}
+}
+
+function getPropertyAssignmentTargetInfo(node: AST.PropertyAssignment) {
+	if (
+		node.initializer.kind === ts.SyntaxKind.BinaryExpression &&
+		node.initializer.operatorToken.kind === ts.SyntaxKind.EqualsToken
+	) {
+		const targetParent = node.initializer.left;
+		return {
+			parenthesizesTarget:
+				targetParent.kind === ts.SyntaxKind.ParenthesizedExpression,
+			replacementNode: node.initializer,
+			targetNode: unwrapParenthesizedNode(targetParent),
+		};
+	}
+
+	const targetNode = unwrapParenthesizedNode(node.initializer);
+
+	return {
+		parenthesizesTarget: false,
+		replacementNode: targetNode,
+		targetNode,
+	};
+}
 
 export default ruleCreator.createRule(typescriptLanguage, {
 	about: {
 		description:
-			"Renames that don't change the identifier name are unnecessary and should use shorthand syntax instead.",
+			"Renames that don't change the identifier name are unnecessary.",
 		id: "unnecessaryRenames",
 		presets: ["stylistic"],
 	},
@@ -26,32 +63,142 @@ export default ruleCreator.createRule(typescriptLanguage, {
 		},
 	},
 	setup(context) {
-		function checkUnnecessaryRename(
-			node: AST.BindingElement | AST.ExportSpecifier | AST.ImportSpecifier,
-			{ sourceFile }: TypeScriptFileServices,
+		function reportUnnecessaryRename(
+			node: AST.AnyNode,
+			parenthesizesTarget: boolean,
+			replacementRange: CharacterReportRange,
+			sourceFile: AST.SourceFile,
+		) {
+			const range = getTSNodeRange(node, sourceFile);
+			const shouldSkipFix =
+				parenthesizesTarget ||
+				countCommentsInRange(sourceFile.text, range) >
+					countCommentsInRange(sourceFile.text, replacementRange);
+
+			context.report({
+				fix: shouldSkipFix
+					? undefined
+					: {
+							range,
+							text: sourceFile.text.slice(
+								replacementRange.begin,
+								replacementRange.end,
+							),
+						},
+				message: "unnecessaryRename",
+				range,
+			});
+		}
+
+		function checkBindingElement(
+			node: AST.BindingElement,
+			sourceFile: AST.SourceFile,
 		) {
 			if (
-				node.propertyName &&
-				ts.isIdentifier(node.propertyName) &&
-				ts.isIdentifier(node.name) &&
-				node.propertyName.text === node.name.text
+				node.propertyName?.kind !== ts.SyntaxKind.ComputedPropertyName &&
+				node.name.kind === ts.SyntaxKind.Identifier &&
+				getModuleNameText(node.propertyName) === getModuleNameText(node.name)
 			) {
-				context.report({
-					fix: {
-						range: getTSNodeRange(node, sourceFile),
-						text: node.name.text,
+				reportUnnecessaryRename(
+					node,
+					false,
+					{
+						begin: node.name.getStart(sourceFile),
+						end: node.getEnd(),
 					},
-					message: "unnecessaryRename",
-					range: getTSNodeRange(node, sourceFile),
-				});
+					sourceFile,
+				);
 			}
+		}
+		function checkExportOrImportSpecifier(
+			original: AST.ExportSpecifier | AST.ImportSpecifier,
+			rangeNode: AST.AnyNode,
+			sourceFile: AST.SourceFile,
+		) {
+			if (
+				getModuleNameText(original.propertyName) !==
+				getModuleNameText(original.name)
+			) {
+				return;
+			}
+
+			const replacementRange = getTSNodeRange(rangeNode, sourceFile);
+
+			reportUnnecessaryRename(original, false, replacementRange, sourceFile);
+		}
+
+		function checkObjectLiteralDestructuring(
+			node: AST.ObjectLiteralExpression,
+			sourceFile: AST.SourceFile,
+		) {
+			for (const property of node.properties) {
+				if (
+					!ts.isPropertyAssignment(property) ||
+					property.name.kind === ts.SyntaxKind.ComputedPropertyName
+				) {
+					continue;
+				}
+
+				const { parenthesizesTarget, replacementNode, targetNode } =
+					getPropertyAssignmentTargetInfo(property);
+
+				if (ts.isObjectLiteralExpression(targetNode)) {
+					checkObjectLiteralDestructuring(targetNode, sourceFile);
+					continue;
+				}
+
+				if (targetNode.kind !== ts.SyntaxKind.Identifier) {
+					continue;
+				}
+
+				const propertyNameText = getModuleNameText(property.name);
+				if (propertyNameText !== targetNode.text) {
+					continue;
+				}
+
+				const replacementRange = getTSNodeRange(replacementNode, sourceFile);
+
+				reportUnnecessaryRename(
+					property,
+					parenthesizesTarget,
+					replacementRange,
+					sourceFile,
+				);
+			}
+		}
+
+		function checkDestructuringAssignment(
+			node: AST.BinaryExpression,
+			sourceFile: AST.SourceFile,
+		) {
+			if (node.operatorToken.kind !== ts.SyntaxKind.EqualsToken) {
+				return;
+			}
+
+			const left = unwrapParenthesizedNode(node.left);
+			if (left.kind !== ts.SyntaxKind.ObjectLiteralExpression) {
+				return;
+			}
+
+			checkObjectLiteralDestructuring(left, sourceFile);
 		}
 
 		return {
 			visitors: {
-				BindingElement: checkUnnecessaryRename,
-				ExportSpecifier: checkUnnecessaryRename,
-				ImportSpecifier: checkUnnecessaryRename,
+				BinaryExpression: (node, { sourceFile }) => {
+					checkDestructuringAssignment(node, sourceFile);
+				},
+				BindingElement: (node, { sourceFile }) => {
+					checkBindingElement(node, sourceFile);
+				},
+				ExportSpecifier: (node, { sourceFile }) => {
+					if (node.propertyName) {
+						checkExportOrImportSpecifier(node, node.propertyName, sourceFile);
+					}
+				},
+				ImportSpecifier: (node, { sourceFile }) => {
+					checkExportOrImportSpecifier(node, node.name, sourceFile);
+				},
 			},
 		};
 	},
