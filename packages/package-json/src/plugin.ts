35baf3a921d69664d750bc32eb1e0eda5444fc02
Josh Goldberg <git@joshuakgoldberg.com>
2026-04-30 20:03:43 -0400

    status: implemented

--- packages/package-json/src/plugin.ts
+++ packages/package-json/src/plugin.ts
@@ -4,6 +4,7 @@ import { directPropertyPresenceRules } from "./directPropertyPresenceRules.ts";
 import { directPropertyValidityRules } from "./directPropertyValidityRules.ts";
 import binNameCasing from "./rules/binNameCasing.ts";
 import privatePresence from "./rules/privatePresence.ts";
+import scriptsNameCasing from "./rules/scriptsNameCasing.ts";
 
 export const packageJson = createPlugin({
 	files: {
@@ -12,6 +13,7 @@ export const packageJson = createPlugin({
 	name: "PackageJSON",
 	rules: [
 		binNameCasing,
+		scriptsNameCasing,
 		privatePresence,
 		...Object.values(directPropertyPresenceRules),
 		...Object.values(directPropertyValidityRules),
