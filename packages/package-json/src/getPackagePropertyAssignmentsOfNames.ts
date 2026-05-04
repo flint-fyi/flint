748579177fa9ff7d1aa90170f2a60ccc7bba650a
Konv Suu <hi@kovsu.com>
2026-05-04 15:30:43 +0800

    feat(package-json): add `dependencyUniqueness` rule

--- packages/package-json/src/getPackagePropertyAssignmentsOfNames.ts
+++ packages/package-json/src/getPackagePropertyAssignmentsOfNames.ts
@@ -1,12 +1,13 @@
-import type { JsonNode, JsonSourceFile } from "@flint.fyi/json-language";
+import type { JsonSourceFile } from "@flint.fyi/json-language";
+import type { AST } from "@flint.fyi/typescript-language";
 import { SyntaxKind } from "typescript";
 
 import { getPackageProperties } from "./getPackageProperties.ts";
 
-export function* getPackagePropertiesOfNames(
+export function* getPackagePropertyAssignmentsOfNames(
 	sourceFile: JsonSourceFile,
 	propertyNames: ReadonlySet<string>,
-) {
+): Iterable<AST.PropertyAssignment & { readonly name: AST.StringLiteral }> {
 	const properties = getPackageProperties(sourceFile);
 	if (!properties) {
 		return;
@@ -18,7 +19,9 @@ export function* getPackagePropertiesOfNames(
 			property.name.kind === SyntaxKind.StringLiteral &&
 			propertyNames.has(property.name.text)
 		) {
-			yield property.initializer as JsonNode;
+			yield property as AST.PropertyAssignment & {
+				readonly name: AST.StringLiteral;
+			};
 		}
 	}
 }
