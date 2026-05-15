165b0abdb3b0c141a93fb2b9829d7a8b0783c54a
Konv Suu <hi@kovsu.com>
2026-05-15 11:35:18 +0800

    update

--- packages/package-json/src/plugin.ts
+++ packages/package-json/src/plugin.ts
@@ -6,9 +6,9 @@ import attribution from "./rules/attribution.ts";
 import binNameCasing from "./rules/binNameCasing.ts";
 import dependencyUniqueness from "./rules/dependencyUniqueness.ts";
 import emptyFields from "./rules/emptyFields.ts";
-import privatePackageProperties from "./rules/privatePackageProperties.ts";
 import exportsSubpathsStyle from "./rules/exportsSubpathsStyle.ts";
 import peerDependenciesInstallation from "./rules/peerDependenciesInstallation.ts";
+import privatePackageProperties from "./rules/privatePackageProperties.ts";
 import privatePresence from "./rules/privatePresence.ts";
 import repositoryShorthand from "./rules/repositoryShorthand.ts";
 import scriptsNameCasing from "./rules/scriptsNameCasing.ts";
@@ -23,9 +23,9 @@ export const packageJson = createPlugin({
 		binNameCasing,
 		dependencyUniqueness,
 		emptyFields,
-		privatePackageProperties,
 		exportsSubpathsStyle,
 		peerDependenciesInstallation,
+		privatePackageProperties,
 		privatePresence,
 		repositoryShorthand,
 		scriptsNameCasing,
