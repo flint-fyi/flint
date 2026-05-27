31b16e56c492025db886435700f663eac5a3690a
Josh Goldberg <git@joshuakgoldberg.com>
2026-05-26 21:31:36 -0400

    Revert "add snapshot for truly empty file"
    This reverts commit 084f1d10e01af0f8ec817c4308ea5cfa6e828802.

--- packages/yaml/src/rules/fileExtensions.test.ts
+++ packages/yaml/src/rules/fileExtensions.test.ts
@@ -4,13 +4,6 @@ import { ruleTester } from "./ruleTester.ts";
 ruleTester.describe(rule, {
 	invalid: [
 		{
-			code: ``,
-			fileName: "config.yml",
-			snapshot: `
-
-Use .yaml extension instead of .yml for YAML files.`,
-		},
-		{
 			code: `
 `,
 			fileName: "config.yml",
