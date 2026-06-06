3410501e3e120addb1dbc5db403bb2ddef16b0b4
Konv Suu <2583695112@qq.com>
2026-06-07 02:02:15 +0800

    fix(jsx): assert rule tester cases contain no language reports (#2891)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

--- packages/jsx/src/rules/ruleTester.ts
+++ packages/jsx/src/rules/ruleTester.ts
@@ -4,7 +4,24 @@ import { RuleTester } from "@flint.fyi/rule-tester";
 import { createRuleTesterTSConfig } from "@flint.fyi/typescript-language";
 
 export const ruleTester = new RuleTester({
-	defaults: { fileName: "file.tsx", files: createRuleTesterTSConfig() },
+	assertNoLanguageReports: true,
+	defaults: {
+		fileName: "file.tsx",
+		files: {
+			...createRuleTesterTSConfig({
+				jsx: "preserve",
+				lib: ["dom", "esnext"],
+			}),
+			"jsx.d.ts": `
+declare namespace JSX {
+	interface IntrinsicElements {
+		[name: string]: Record<string, unknown>;
+	}
+}
+`,
+		},
+	},
 	describe,
+	diskBackedFSRoot: import.meta.dirname,
 	it,
 });
