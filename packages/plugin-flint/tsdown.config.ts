02e63a4ff84f5d575a6777f810f8c31f7c8262e6
michael faith <michaelfaith@users.noreply.github.com>
2026-07-18 19:40:22 -0500

    chore: create new build package
    This change creates a new internal `build` package, to house our base tsconfig and tsdown config.  We could have additional build needs in the future that might be a good fit here too.

--- packages/plugin-flint/tsdown.config.ts
+++ packages/plugin-flint/tsdown.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from "tsdown";
 
+import { base } from "@flint.fyi/build/tsdown";
+
 export default defineConfig({
-	attw: {
-		enabled: "ci-only",
-		profile: "esm-only",
-	},
-	clean: ["./node_modules/.cache/tsbuild/"],
-	dts: { build: true, incremental: true },
-	entry: ["src/index.ts"],
-	exports: {
-		devExports: true,
-		packageJson: false,
-	},
-	failOnWarn: true,
-	fixedExtension: false,
-	outDir: "lib",
-	unbundle: false,
+	...base,
 });
--- packages/svelte-language/tsdown.config.ts
+++ packages/svelte-language/tsdown.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "tsdown";
 
+import { base } from "@flint.fyi/build/tsdown";
+
 export default defineConfig({
-	attw: {
-		enabled: "ci-only",
-		profile: "esm-only",
-	},
-	clean: ["./node_modules/.cache/tsbuild/"],
+	...base,
 	deps: {
 		// [MISSING_EXPORT] Warning: "AST" is not exported by "../../node_modules/.pnpm/svelte@5.54.0/node_modules/svelte/types/index.d.ts"
 		// https://github.com/sveltejs/svelte/issues/17520
 		// https://github.com/sxzz/rolldown-plugin-dts/issues/170
 		neverBundle: ["svelte/compiler"],
 	},
-	dts: { build: true, incremental: true },
-	entry: ["src/index.ts"],
-	exports: {
-		devExports: true,
-		packageJson: false,
-	},
-	failOnWarn: true,
-	fixedExtension: false,
-	outDir: "lib",
-	unbundle: true,
 });
