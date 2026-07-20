d021f25609976ea61c7dcfcce2862db3b951b56b
michael faith <michaelfaith@users.noreply.github.com>
2026-07-20 13:10:54 -0500

    feat: begin bundling entry points
    This change updates our tsdown config to lean more into default behavior, resulting in the following changes:
    - all entry points are now bundled
    - the output directory for build files is now `dist`.  incremental build output from typescript is still going to the `lib` dir, so the two don't step on each other's toes
    - removed the explicit config to build dts (default behavior) and `devExports` condition (we removed these a while ago)
    
    Build times are considerably faster now.

--- packages/build/src/tsdown/base.ts
+++ packages/build/src/tsdown/base.ts
@@ -6,14 +6,6 @@ export const base: UserConfig = {
 		profile: "esm-only",
 	},
 	clean: ["./node_modules/.cache/tsbuild/"],
-	dts: { build: true, incremental: true },
 	entry: ["src/index.ts"],
-	exports: {
-		devExports: true,
-		packageJson: false,
-	},
 	failOnWarn: true,
-	fixedExtension: false,
-	outDir: "lib",
-	unbundle: true,
 };
