240550f9b6d8af72f889e477e42f1278655eabd4
michael faith <michaelfaith@users.noreply.github.com>
2026-08-08 15:35:52 -0500

    chore: adopt `isolatedDeclarations` in `core`
    Contributes to (but doesn't close) #3143
    
    This change adopts `isolatedDeclarations` in the `core` package.

--- packages/core/src/host/watcher.ts
+++ packages/core/src/host/watcher.ts
@@ -1,9 +1,12 @@
 export const gitVcs = "/.git";
 export const jjVcs = "/.jj";
-export const vcsDirectories = [gitVcs, jjVcs];
+export const vcsDirectories: string[] = [gitVcs, jjVcs];
 export const nodeModulesDir = "/node_modules";
 export const nodeModulesCache = "/node_modules/.cache";
-export const commonlyIgnoredPaths = [...vcsDirectories, nodeModulesDir];
-export const commonlyIgnoredGlobs = commonlyIgnoredPaths.map(
+export const commonlyIgnoredPaths: string[] = [
+	...vcsDirectories,
+	nodeModulesDir,
+];
+export const commonlyIgnoredGlobs: string[] = commonlyIgnoredPaths.map(
 	(dir) => `**${dir}`,
 );
