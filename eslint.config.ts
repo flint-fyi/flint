a9b322ced171f330d6d2b8f481e9fe71f63ae3d6
michael faith <michaelfaith@users.noreply.github.com>
2026-08-13 16:56:42 -0500

    chore: migrate all configs to be `isolatedDeclarations` compatible

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,7 +10,7 @@ import packageJson from "eslint-plugin-package-json/experimental";
 import perfectionist from "eslint-plugin-perfectionist";
 import * as regexp from "eslint-plugin-regexp";
 import yml from "eslint-plugin-yml";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores, type ConfigObject } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 // https://typescript-eslint.io/troubleshooting/typed-linting/performance#importextensions-enforcing-extensions-are-not-used
@@ -36,7 +36,7 @@ function banJsImportExtension() {
 	];
 }
 
-export default defineConfig(
+const config: ConfigObject[] = defineConfig(
 	globalIgnores([
 		"**/*.snap",
 		"**/node_modules",
@@ -250,3 +250,5 @@ export default defineConfig(
 		},
 	},
 );
+
+export default config;
