116a836e415155a326a6f2ce80786aeb4e1b84e6
Konv Suu <2583695112@qq.com>
2026-08-18 22:02:04 +0800

    perf(core): cache selection matcher regular expressions (#3247)

--- packages/core/src/directives/createSelectionMatcher.ts
+++ packages/core/src/directives/createSelectionMatcher.ts
@@ -1,6 +1,12 @@
+import { CachedFactory } from "cached-factory";
+
+const matchers = new CachedFactory(
+	(selection: string) => new RegExp(`^${selection.replaceAll("*", ".*")}$`),
+);
+
 // TODO: There's got to be a better way.
 // Maybe an existing common one like minimatch?
 // https://github.com/flint-fyi/flint/issues/245
 export function createSelectionMatcher(selection: string): RegExp {
-	return new RegExp(`^${selection.replaceAll("*", ".*")}$`);
+	return matchers.get(selection);
 }
