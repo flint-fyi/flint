9ddb33a2b3b6fdcca922a937be4c1f674a99e3ae
Josh Goldberg <git@joshuakgoldberg.com>
2026-08-29 14:17:58 -0400

    lil fix

--- packages/performance-testing/src/calculateDelta.ts
+++ packages/performance-testing/src/calculateDelta.ts
@@ -1,3 +1,11 @@
+export function calculateDelta(eslint: string, flint: string): string {
+	const eslintDuration = parseDuration(eslint);
+	const delta =
+		((parseDuration(flint) - eslintDuration) / eslintDuration) * 100;
+
+	return `${delta > 0 ? "+" : ""}${delta.toFixed(1)}%`;
+}
+
 function parseDuration(result: string) {
 	const match = /^([0-9.]+)\s+(m?s)/.exec(result);
 	if (!match) {
@@ -6,12 +14,4 @@ function parseDuration(result: string) {
 
 	const duration = Number(match[1]);
 	return match[2] === "s" ? duration * 1000 : duration;
-}
-
-export function calculateDelta(eslint: string, flint: string): string {
-	const eslintDuration = parseDuration(eslint);
-	const delta =
-		((parseDuration(flint) - eslintDuration) / eslintDuration) * 100;
-
-	return `${delta > 0 ? "+" : ""}${delta.toFixed(1)}%`;
 }
